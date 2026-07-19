1f68d1ccc86c78e70153d1243c9bf5bdcdb1fe2f
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-07-20 01:55:22 +0500

    feat (transcription): initial version

---
+++ test_ears.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+while True:
+
+    try:
+        with sr.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+            text = recognizer.recognize_google(audio) # type: ignore
+            print(text)
+    except:
+        pass
