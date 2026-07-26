3ed29fa252257bc769c6cb29e229fbfbd3a23a80
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-07-27 00:59:55 +0500

    feat(stt): add push-to-talk

--- test_ears.py
+++ test_ears.py
@@ -1,22 +1,49 @@
 import speech_recognition as sr
+import keyboard as k
+import time
 
-def listen_neural():
+from test_piper import speak_neral
+
+def listen_neural() -> str:
     recognizer = sr.Recognizer()
+    mic = sr.Microphone()
+    
+    print("\nJarvis: Hold 'M' to speak...")
 
+    while not k.is_pressed("m"):
+        time.sleep(0.05)
+        
+    print("Jarvis: Listening... (Release 'M' to finish)")
+
+    k.block_key("m")
     try:
-        with sr.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            print("Jarvis: Listening...")
-            audio = recognizer.listen(mic, timeout=5, phrase_time_limit=8)
-    except sr.WaitTimeoutError:
-        print("Jarvis: No speech detected.")
+        with mic as source:
+            recognizer.adjust_for_ambient_noise(source, duration=0.1)
+            
+            audio_chunks = []
+            stream = mic.stream
+            
+            while k.is_pressed('m'):
+                try:
+                    chunk = stream.read(source.CHUNK)
+                    audio_chunks.append(chunk)
+                except IOError:
+                    continue
+    finally:
+        k.unblock_key("m")
+    print("Jarvis: Processing transcription...")
+    
+    if not audio_chunks:
         return ""
+        
+    raw_audio_bytes = b"".join(audio_chunks)
+    audio_data = sr.AudioData(raw_audio_bytes, source.SAMPLE_RATE, source.SAMPLE_WIDTH)
 
     try:
-        return recognizer.recognize_google(audio) # type: ignore
+        return recognizer.recognize_google(audio_data).strip()
     except sr.UnknownValueError:
         print("Jarvis: I did not catch that.")
         return ""
     except sr.RequestError:
-        print("Jarvis: Speech recognition is unavailable.")
+        print("Jarvis: Speech recognition service is unavailable.")
         return ""
