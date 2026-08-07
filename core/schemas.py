a3a8e455a5d1b61beb034636148c5976b0910bd7
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-08-08 00:20:27 +0500

    docs(core/schemas): add comments for better code readability
    refractor(core/schemas): update the order of commands in IntentType to be listed alphabetically

--- core/schemas.py
+++ core/schemas.py
@@ -1,21 +1,29 @@
+# Imports
 from pydantic import BaseModel, Field
 from typing import Literal
 
+# IntentType defines the possible intents for commands that can be executed
 IntentType = Literal[
+    # Git commands
     "git_commit",
     "git_create_branch",
-    "git_switch_branch",
+    "git_current_branch",
     "git_delete_branch",
     "git_list_branches",
-    "git_current_branch",
-    "spotify_play",
-    "spotify_pause",
-    "spotify_skip",
-    "spotify_previous",
+    "git_switch_branch",
+
+    # Spotify commands
     "spotify_info",
+    "spotify_pause",
+    "spotify_play",
+    "spotify_previous",
+    "spotify_skip",
+
+    # General commands
     "general_chat"
 ]
 
+# Structure of the command payload that will be recieved by the API
 class CommandPayload(BaseModel):
     intent: IntentType = Field(..., description="The intent of the command")
     query: str = Field(..., description="The command to be executed")
